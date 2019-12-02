572354ca7a75a4ce37f774840d5485d5d8f29b85
zhouquanbiao1991 <zhouquanbiao1991@163.com>
2019-12-02 21:11:28 +0800

    block chain

--- MyFirstBlockChain/MyFirstBlockChain/MyFirstBlockChain.py
+++ MyFirstBlockChain/MyFirstBlockChain/MyFirstBlockChain.py
@@ -21,7 +21,7 @@ class MyBlockChain:
         self.chain.append(block)
         return block
 
-    def new_trans():
+    def new_trans(sender, recipient, amount):
         self.current_trans.append(
             {
                 'sender':sender,
@@ -49,7 +49,56 @@ class MyBlockChain:
         guess_hash = hashlib.sha256(guess).hexdigest()
         return guess_hash[:4] == '0000'
 
-app_flask = app(__name__)
+app = Falsk(__name__)
+
+blockchain = MyBlockChain()
+
+node_identifier = str(uuid4()).replace('-','')
+
+@app.route('/mine', method=['GET'])
+def mine():
+    last_block = blockchain.chain[-1]
+    last_proof = last_block.proof
+    proof = blockchain.proof_of_work(last_proof)
+
+    new_trans(sender=0, recipient=node_identifier, amount=1)
+
+    block = new_block(proof=proof, previous_hash=blockchain.hash(last_block))
+
+    response = {
+        'message': "New Block Forged",
+        'index': block['index'],
+        'transactions': block['transactions'],
+        'proof': block['proof'],
+        'previous_hash': block['previous_hash'],
+    }
+    return jsonify(response), 200
+
+# post data format:
+# {
+#    sender: xxxx,
+#    recipient: xxxx,
+#    amount: xxxx,
+# }
+@app.route('/transcation/new', method=['POST'])
+def new_transcation():
+    values = request.get_json()
+    required = ['sender', 'recipient', 'amount']
+    if not all(k in values for k in required):
+        return 'missing value', 400
+    index = new_trans(sender=values['sender'], 
+                      recipient=values['recipient'], 
+                      amount=values['amount'])
+    response = 'we will add a new trancation index:' + index
+    return jsonify(response), 201
+
+@app.route('/full_chain', method=['GET'])
+def full_chain():
+    response = {
+        'chain':blockchain.chain,
+        'length':len(blockchain.chain),
+    }
+    return jsonify(response),200
 
 
 if __name__ == '__main__':
